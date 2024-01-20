bd5fb05f23d75bf2f2d2cd30d951d9d0db3cb7f8
Nuria Gîrz <156543261+girz-nuria@users.noreply.github.com>
2024-01-20 23:11:28 +0200

    Add files via upload

---
+++ 16/16/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numere = new int[5];
+
+            Console.WriteLine("Introduceti cele 5 numere:");
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write($"Numarul {i + 1}: ");
+                numere[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Array.Sort(numere);
+
+            Console.WriteLine("Numerele în ordine crescatoare:");
+
+            foreach (int numar in numere)
+            {
+                Console.WriteLine(numar);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 8/8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Dati o valoare lui a:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Dati o valoare lui b:");
+            b = Convert.ToInt32(Console.ReadLine());
+            a = a * b;
+            b = a / b;
+            a = a / b;
+            Console.Write("a="); Console.WriteLine(a);
+            Console.Write("b="); Console.WriteLine(b);
+            Console.ReadLine ();
+        }
+    }
+}
